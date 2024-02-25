f756f008c70646efc38263624d266e2de329eeef
Pelin Caliskanlar <161216933+Pelin2008@users.noreply.github.com>
2024-02-25 16:31:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+meme_dict = {
+            
+            "CRINGE" : "garip ya da utandırıcı bir şey",
+            "ROFL" : "bir şakaya karşılık cevap",
+            "SHEESH ": "onaylamamak",
+            "CREEPY" :"korkunç",
+            "AGGRO" :"agresifleşmek/sinirlenmek",
+            "BAD" : "kötü bir şey"    ,      
+            } 
+    #values
+for i in meme_dict.keys ():
+    print(i)
